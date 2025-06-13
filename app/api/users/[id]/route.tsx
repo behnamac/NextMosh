761bc1c0beb8fed148b45e8163e764820f87dca7
Behnam Sepehri <behnam.sep@gmail.com>
2025-06-13 14:39:32 +0200

    feat: add validation schema for user data and update PUT method to use it

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { schema } from "../schema";
 
 interface Props {
   params: {
@@ -18,8 +19,9 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 export async function PUT(request: NextRequest, { params: { id } }: Props) {
   // Validate the request body
   const body = await request.json();
-  if (!body.name) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
   if (id > 10) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });

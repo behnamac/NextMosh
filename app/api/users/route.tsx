d3f1a30093425217f13e64b1609561cb9768db8e
Behnam Sepehri <behnam.sep@gmail.com>
2025-06-13 14:43:22 +0200

    feat: add validation to POST method for user creation

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { schema } from "./schema";
+import { id } from "zod/v4/locales";
 
 export function GET(request: NextRequest) {
   return NextResponse.json([
@@ -9,6 +11,10 @@ export function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  // You can add logic to save the user here
-  return NextResponse.json(body);
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
